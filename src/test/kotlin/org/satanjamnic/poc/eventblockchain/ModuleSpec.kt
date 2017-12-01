28621e2bd5f07a259ca1bf51498140ea9a585faf
satanjamnic <satanjamnic@gmail.com>
2017-12-01 06:10:36 +0100

    next TDD scenario

--- src/test/kotlin/org/satanjamnic/poc/eventblockchain/ModuleSpec.kt
+++ src/test/kotlin/org/satanjamnic/poc/eventblockchain/ModuleSpec.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import org.satanjamnic.poc.eventblockchain.businessprocess.BaseBusinessAction
 import org.satanjamnic.poc.eventblockchain.businessprocess.step.Create
 import org.satanjamnic.poc.eventblockchain.businessprocess.step.Process
+import org.satanjamnic.poc.eventblockchain.event.BaseEvent
 import org.satanjamnic.poc.eventblockchain.event.queue.EventQueue
 import org.satanjamnic.poc.eventblockchain.event.type.EventType
 import org.satanjamnic.poc.eventblockchain.module.BaseModule
@@ -52,7 +53,7 @@ class ModuleSpec {
         val createPolicyBusinessProcess = BaseBusinessAction("create policy")
 
         val policyManagement = BaseModule("PolicyManagement", listOf(Create(createPolicyBusinessProcess, "PolicyCreated")))
-        val billing = BaseModule("Billing", listOf(Process(createPolicyBusinessProcess, "PolicyCreated", "PolicyAccountCreated")))
+        val billing = BaseModule("Billing", listOf(Process(createPolicyBusinessProcess, "PolicyCreated", "AccountCreated")))
         val registries = BaseModule("Registries", listOf(Process(createPolicyBusinessProcess, "PolicyCreated", "RegistryCreated")))
 
         val policyCreatedQueue = EventQueue("PolicyCreated")
@@ -60,7 +61,7 @@ class ModuleSpec {
         policyCreatedQueue.registerListener(registries)
         policyManagement.publishesTo(policyCreatedQueue)
 
-        val policyAccountCreatedQueue = EventQueue("PolicyAccountCreated")
+        val policyAccountCreatedQueue = EventQueue("AccountCreated")
         billing.publishesTo(policyAccountCreatedQueue)
 
         val registryCreatedQueue = EventQueue("RegistryCreated")
@@ -71,7 +72,33 @@ class ModuleSpec {
 
         // then
         assert(policyCreatedQueue.listEvents().find { it.type() == EventType("PolicyCreated") } != null)
-        assert(policyAccountCreatedQueue.listEvents().find { it.type() == EventType("PolicyAccountCreated") } != null)
+        assert(policyAccountCreatedQueue.listEvents().find { it.type() == EventType("AccountCreated") } != null)
         assert(registryCreatedQueue.listEvents().find { it.type() == EventType("RegistryCreated") } != null)
     }
+
+//    @Test
+//    fun shouldNotMixTwoPolicyCreationEvents() {
+//        // given
+//        val businessAction = BaseBusinessAction("create policy")
+//
+//        val policyManagement = BaseModule("PolicyManagement", listOf(Create(businessAction, "PolicyCreated")))
+//        val billing = BaseModule("Billing", listOf(Process(businessAction, "PolicyCreated", "AccountCreated")))
+//
+//        val policyCreatedQueue = EventQueue("PolicyCreated")
+//        policyCreatedQueue.registerListener(billing)
+//        policyManagement.publishesTo(policyCreatedQueue)
+//
+//        val policyAccountCreatedQueue = EventQueue("AccountCreated")
+//        billing.publishesTo(policyAccountCreatedQueue)
+//
+//        // when
+//        policyManagement.process(businessAction)
+//        policyManagement.process(businessAction)
+//
+//        // then
+//        assert(policyCreatedQueue.listEvents().find { it == BaseEvent("PolicyCreated", 1) } != null)
+//        assert(policyCreatedQueue.listEvents().find {it == BaseEvent("PolicyCreated", 2)} != null)
+//        assert(policyCreatedQueue.listEvents().find {it == BaseEvent("AccountCreated", 1) && it.createdFrom(BaseEvent("PolicyCreated",1))} != null)
+//        assert(policyCreatedQueue.listEvents().find {it == BaseEvent("AccountCreated", 2) && it.createdFrom(BaseEvent("PolicyCreated",1)} != null)
+//    }
 }
