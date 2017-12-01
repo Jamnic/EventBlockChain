37fcd1341c3b8fc9d0946527f91db405a21ddc9d
satanjamnic <satanjamnic@gmail.com>
2017-12-01 05:47:40 +0100

    Removal of ModuleFactory

--- src/main/kotlin/org/satanjamnic/poc/eventblockchain/module/factory/ModuleFactory.kt
+++
@@ -1,8 +0,0 @@
-package org.satanjamnic.poc.eventblockchain.module.factory
-
-import org.satanjamnic.poc.eventblockchain.module.Module
-import org.satanjamnic.poc.eventblockchain.ModuleReceipt
-
-interface ModuleFactory {
-    fun create(moduleReceipt: ModuleReceipt): Module
-}
